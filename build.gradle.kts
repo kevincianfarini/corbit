7620187df9ee1bd0d86e3e39001792179cd06729
Kevin Cianfarini <kevincianfarini@gmail.com>
2020-04-22 15:36:12 -0400

    Add ktlint enforcement

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+plugins {
+    kotlin("multiplatform") version "1.3.72"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+}
+
 allprojects {
     repositories {
         jcenter()
     }
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
--- corbit-bencoding/src/commonMain/kotlin/com.kevincianfarini/BencodedData.kt
+++ corbit-bencoding/src/commonMain/kotlin/com.kevincianfarini/BencodedData.kt
@@ -7,9 +7,9 @@ data class BencodedInt(val value: Long) : BencodedData()
 data class BencodedString(val value: ByteArray) : BencodedData()
 
 data class BencodedList(
-        val value: List<BencodedData>
+    val value: List<BencodedData>
 ) : BencodedData(), List<BencodedData> by value
 
 data class BencodedDict(
-        val value: Map<BencodedString, BencodedDict>
+    val value: Map<BencodedString, BencodedDict>
 ) : BencodedData(), Map<BencodedString, BencodedData> by value
